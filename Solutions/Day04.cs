205711474a37295b48d47624162520e5cd75aacb
Wouter de Bruin <wouterdebruin88@gmail.com>
2023-12-04 22:29:11 +0100

    day4b but performing

--- Solutions/Day04.cs
+++ Solutions/Day04.cs
@@ -7,17 +7,18 @@ namespace Advent.Solutions;
 
 public class Day04 : DayBase
 {
-    public class Card 
+    public class Card
     {
-        public int i;
+        public int idx;
+        public int Copies = 1;
         public List<int> Winning = new();
         public List<int> Mine = new();
-        public int n;
+        public int Score;
     }
 
     public override void Run()
     {
-        var lines = ReadInput("Day04.txt");        
+        var lines = ReadInput("Day04.txt");
         int answer = 0;
 
         List<Card> cards = new();
@@ -28,36 +29,32 @@ public class Day04 : DayBase
             cards.Add(c);
         }
 
-        int j = 0;
-        while(j < cards.Count)
+        for(int i = 0; i < cards.Count; i++)
         {
-            var gamenum = cards[j].i;
-            var nToAdd = cards[j].n;
-            
-            for(int a = 1; a <= nToAdd; a++)
+            if(cards[i].Score > 0)
             {
-                if(gamenum+a < lines.Length)
+                for(int j=1; j<=cards[i].Score; j++)
                 {
-                    var c = GetCard(lines, gamenum+a);
-                    cards.Insert(cards.LastIndexOf(cards.Last(c => c.i == gamenum+a)), c);
+                    if(i+j < cards.Count())
+                    {
+                        cards[i+j].Copies += cards[i].Copies;
+                    }
                 }
             }
-
-            j++;
         }
 
-        answer = cards.Count;
-        WriteLine($"{answer}");        
+        answer = cards.Sum(x => x.Copies);
+        WriteLine($"{answer}");
     }
 
     private static Card GetCard(string[] lines, int i)
     {
         Card c = new();
-        c.i = i;
+        c.idx = i;
         string[] cardParts = lines[i].Split(':')[1].Split('|');
         c.Winning = cardParts[0].Split(' ').ToList().Where(x => x != "").ToArray().ToIntArray().ToList();
         c.Mine = cardParts[1].Split(' ').ToList().Where(x => x != "").ToArray().ToIntArray().ToList();
-        c.n = c.Winning.Where(w => c.Mine.Any(y => y == w)).Count();
+        c.Score = c.Winning.Where(w => c.Mine.Any(y => y == w)).Count();
         return c;
     }
 }
